0751a534e6fed82f2a61cea5f01c02f9f6cb915d
Raul Burgos Murray <specializeddevel@gmail.com>
2024-09-13 18:20:39 -0300

    Algorithm finished

--- binary_search/src/Main.java
+++ binary_search/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args){
+        //int[] originalArray = {1, 2, 3,4,5};
+        int[] originalArray = new int[100000000];
+        for(int i=0; i<100000000; i++){
+            originalArray[i] = i+1;
+        }
+        System.out.println(binarySearch(originalArray, 4900));
     }
+
+    public static int binarySearch(int[] originalArray, int valueToSearch){
+        var startIndex = 0;
+        var endIndex = originalArray.length - 1;
+        int mid = 0;
+        int operacion = 0;
+        while(startIndex <= endIndex){
+            mid = (endIndex+startIndex)/2;
+            long currentValue = originalArray[mid];
+            if(currentValue == valueToSearch){
+               return mid;
+            } else if (currentValue > valueToSearch) {
+                endIndex =  mid-1;
+            } else {
+                startIndex = mid+1;
+            }
+            operacion++;
+            System.out.println("operacion #" + operacion);
+        }
+        return -1;
+    }
+
+
 }
